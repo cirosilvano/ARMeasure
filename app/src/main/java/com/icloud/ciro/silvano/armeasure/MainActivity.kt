8055637db8d00fe543b7de3723bad4ff7e3d2407
Ciro Silvano <ciro.silvano@icloud.com>
2022-05-13 15:10:40 +0200

    Initial commit

---
+++ app/src/main/java/com/icloud/ciro/silvano/armeasure/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.icloud.ciro.silvano.armeasure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
